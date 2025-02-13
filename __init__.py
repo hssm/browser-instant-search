34c17ccb05f27f8fb69e4d136e667d0a1555490a
hssm <houssam.salem.au@gmail.com>
2025-02-13 17:52:45 +1100

    Preserve search history with a debounce

--- __init__.py
+++ __init__.py
@@ -1,13 +1,16 @@
 # https://github.com/hssm/browser-instant-search
 # Version 1.0
 
-from aqt import gui_hooks
+from aqt import *
 from aqt.theme import theme_manager
-
+from threading import Thread
+import time
 
 class InstantSearch:
+
     def __init__(self):
         self.last_search = ''
+        self.save_at = 0
 
     def browser_will_show(self, browser):
         self.browser = browser
@@ -21,11 +24,13 @@ class InstantSearch:
         text = self.browser.current_search()
         if text != self.last_search:
             try:
+                self.last_search = text
                 normed = self.browser.col.build_search_string(text)
-                self.last_search = normed
                 self.browser.table.search(normed)
                 self.browser.form.searchEdit.setStyleSheet("")
+                self.maybe_save_history(text)
             except Exception as err:
+                self.save_at = None
                 # This is breaking the browser UI ??? This has to be a qt bug.
                 # if theme_manager.night_mode:
                 #     self.browser.form.searchEdit.setStyleSheet("QComboBox {background-color: #4a3a36;}")
@@ -33,11 +38,42 @@ class InstantSearch:
                 #     self.browser.form.searchEdit.setStyleSheet("QComboBox {background-color: #ffc9b9;}")
                 pass
 
+    def maybe_save_history(self, text):
+        """Schedule for saving with a debounce"""
+        if not text:
+            self.save_at = None
+            return
+        saver = Thread(target=self.update_history, args=[text])
+        self.save_at = time.time() + 2
+        saver.start()
+
+    def update_history(self, text):
+        time.sleep(2)
+        if not self.save_at or time.time() < self.save_at:
+            # Another thread has superseded this one
+            return
+        if self.browser not in aqt.DialogManager._dialogs[self.browser.__class__.__name__]:
+            # Browser was closed
+            return
+        self.browser.form.searchEdit.blockSignals(True)
+        # Based on browser.py::update_history()
+        sh = self.browser.mw.pm.profile.get("searchHistory", [])
+        if text in sh:
+            sh.remove(text)
+        sh.insert(0, text)
+        sh = sh[:30]
+        # The clear() resets the cursor position so we restore it ourselves
+        pos = self.browser.form.searchEdit.lineEdit().cursorPosition()
+        self.browser.form.searchEdit.clear()
+        self.browser.form.searchEdit.addItems(sh)
+        self.browser.form.searchEdit.lineEdit().setCursorPosition(pos)
+        self.browser.mw.pm.profile["searchHistory"] = sh
+        self.browser.form.searchEdit.blockSignals(False)
+
     def on_current_index_changed(self, index):
         """Do a search on drop-down selection. -1 is text edit. Skip those as we handle already"""
         if index >= 0:
             self.on_text_edited()
-
 
 _is = InstantSearch()
 
